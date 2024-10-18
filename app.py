ba924fb5e0f8fc5b9b6f43d78b5d4a5ccddc412a
fernandoorosco <fernandooroscobianchi@gmail.com>
2024-10-18 13:24:28 -0300

    tabuada

--- app.py
+++ app.py
@@ -43,6 +43,27 @@ def area(largura: float, comprimento: float):
 def potencial(n1: float, n2: float):
     return f"""<h1>O primeiro numero {n1} elevado ao segundo numero {n2} é igual a {n1**n2} <h1>"""
 
+@app.route("/tabuada/<float:numero>",methods=('GET',))
+def tabuada(numero: float):
+    if numero > 0:
+        return f"""<p>A tabuada do numero {numero}:<p>
+        <ul><li><p> {numero} * 1 = {numero*1} </p></ul></li>
+        <ul><li><p> {numero} * 2 = {numero*2} </p></ul></li> 
+        <ul><li><p> {numero} * 3 = {numero*3} </p></ul></li> 
+        <ul><li><p> {numero} * 4 = {numero*4} </p></ul></li> 
+        <ul><li><p> {numero} * 5 = {numero*5} </p></ul></li> 
+        <ul><li><p> {numero} * 6 = {numero*6} </p></ul></li> 
+        <ul><li><p> {numero} * 7 = {numero*7} </p></ul></li> 
+        <ul><li><p> {numero} * 8 = {numero*8} </p></ul></li> 
+        <ul><li><p> {numero} * 9 = {numero*9} </p></ul></li> 
+        <ul><li><p> {numero} * 10 = {numero*10} </p></ul></li>
+        """
+    
+    else:
+        return "<p>Digite um número válido</p>"
+
+
+
  
     
 
